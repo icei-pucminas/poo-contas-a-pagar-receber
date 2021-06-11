d39eb083067e2ed422adca1f7a64a2f12de508b6
Gustavo Caldeira <caldeira.santos@gmail.com>
2021-06-11 18:49:42 -0300

    Remoção imports

--- ContasPagarReceber/Src/ContasPagarReceber.WindowsForm/FormContas.cs
+++ ContasPagarReceber/Src/ContasPagarReceber.WindowsForm/FormContas.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ContasPagarReceber.WindowsForm
@@ -85,7 +79,7 @@ namespace ContasPagarReceber.WindowsForm
             gridTransacoes.Refresh();
         }
 
-	private void label1_Click_2(object sender, EventArgs e)
+	    private void label1_Click_2(object sender, EventArgs e)
         {
 
         }
